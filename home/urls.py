d303eb0f0fecb7dd86da6be47191f51695578f7c
Stamos Martin <stajama@yahoo.com>
2018-12-07 10:28:08 -0600

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     # url(r'$', views.mainView),
-    path(r'<int:id>/<str:geoX>/<str:geoY>/<action>', views.log),
-    url(r'', views.getID),
+    # path(r'<int:id>/<str:geoX>/<str:geoY>/<action>', views.log),
+    # url(r'', views.getID),
     
 ]
